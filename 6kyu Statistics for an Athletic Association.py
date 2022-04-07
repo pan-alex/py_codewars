50e2a48c03b38e60cb2738da05b0a02e49fdb1ff
pan-alex <alexanderypan@gmail.com>
2022-04-07 17:28:25 -0400

    Create

---
+++ 6kyu Statistics for an Athletic Association.py
@@ -0,0 +1,58 @@
+# https://www.codewars.com/kata/55b3425df71c1201a800009c
+# You are the "computer expert" of a local Athletic Association (C.A.A.). Many teams of runners come to compete. Each time you get a string of all race results of every team who has run. For example here is a string showing the individual results of a team of 5 runners:
+#
+# "01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17"
+#
+# Each part of the string is of the form: h|m|s where h, m, s (h for hour, m for minutes, s for seconds) are positive or null integer (represented as strings) with one or two digits. Substrings in the input string are separated by , or ,.
+#
+# To compare the results of the teams you are asked for giving three statistics; range, average and median.
+#
+#     Range : difference between the lowest and highest values. In {4, 6, 9, 3, 7} the lowest value is 3, and the highest is 9, so the range is 9 − 3 = 6.
+#
+#     Mean or Average : To calculate mean, add together all of the numbers in a set and then divide the sum by the total count of numbers.
+#
+#     Median : In statistics, the median is the number separating the higher half of a data sample from the lower half. The median of a finite list of numbers can be found by arranging all the observations from lowest value to highest value and picking the middle one (e.g., the median of {3, 3, 5, 9, 11} is 5) when there is an odd number of observations. If there is an even number of observations, then there is no single middle value; the median is then defined to be the mean of the two middle values (the median of {3, 5, 6, 9} is (5 + 6) / 2 = 5.5).
+#
+# Your task is to return a string giving these 3 values. For the example given above, the string result will be
+#
+# "Range: 00|47|18 Average: 01|35|15 Median: 01|32|34"
+#
+# of the form: "Range: hh|mm|ss Average: hh|mm|ss Median: hh|mm|ss"`
+#
+# where hh, mm, ss are integers (represented by strings) with each 2 digits.
+# Remarks:
+#
+#     if a result in seconds is ab.xy... it will be given truncated as ab.
+#     if the given string is "" you will return ""
+#
+
+# Process string into list of runner times
+    # Process individual runner times into floats
+
+# With list of runner times as floats: Find the range, mean, and median
+
+# Convert these values back to string format.
+
+import statistics
+
+def stat(runners):
+    # your code
+    list_of_strings = runners.split(', ')
+    list_of_times = [string_to_seconds(string) for string in list_of_strings]
+    mean = seconds_to_string(statistics.mean(list_of_times))
+    median = seconds_to_string(statistics.median(list_of_times))
+    range = seconds_to_string(max(list_of_times) - min(list_of_times))
+    return f'Range: {range} Average: {mean} Median: {median}'
+
+
+def string_to_seconds(string):
+    times = string.split('|')
+    time_in_seconds = int(times[0])*3600 + float(times[1])*60 + float(times[2])
+    return time_in_seconds
+
+
+def seconds_to_string(seconds):
+    hours = str(int(seconds // 3600))
+    minutes = str(int(seconds % 3600 // 60))
+    seconds = str(int(seconds % 3600 % 60))
+    return f'{hours.zfill(2)}|{minutes.zfill(2)}|{seconds.zfill(2)}'
